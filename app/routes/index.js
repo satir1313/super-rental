70d220a37cdf50ad9c7d0673ceceab46f1a3a7ca
shahabj13@gmail.com <satir13@satir13.satir13>
2020-08-21 19:41:54 +0800

    part two_details page

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,7 +13,7 @@ export default class IndexRoute extends Route {
     let { data } = await response.json();
 
     return data.map(model => {
-      let { attributes } = model;
+      let { id, attributes } = model;
       let type;
 
       if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
@@ -22,7 +22,7 @@ export default class IndexRoute extends Route {
         type = 'Standalone';
       }
 
-      return { type, ...attributes };
+      return { id, type, ...attributes };
     });
   }
 
